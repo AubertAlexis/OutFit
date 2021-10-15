b8fbc34d21277d225bcf0346262200611b4e183a
LucasBur <lucas.burette@edu.itescia.fr>
2021-10-15 16:55:42 +0200

    Complete CRUD for category (#15)

---
+++ src/Controller/Admin/Category/Index.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Admin\Category;
+
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Index extends AbstractController
+{
+    /**
+     * @Route("/category", name="admin_category_index", methods={"GET"})
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('admin/category/index.html.twig', [
+            'datatable' => true,
+            'categories' => $categoryRepository->findAll()
+        ]);
+
+    }
+}
