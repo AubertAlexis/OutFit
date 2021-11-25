88dcc5975da5b4dcc91eed7d3b1b58ca669d79ce
Lucas <lucas.burette@edu.itescia.fr>
2021-11-25 16:34:57 +0100

    User personnal information form filled

--- src/Controller/UI/Account/Personal.php
+++ src/Controller/UI/Account/Personal.php
@@ -3,7 +3,12 @@ declare(strict_types=1);
 
 namespace App\Controller\UI\Account;
 
+use App\Entity\Customer;
+use App\Form\CustomerType;
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +17,24 @@ class Personal extends AbstractController
 
     /**
      * @Route("/mon-compte/informations", name="ui_account_personal")
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param CustomerRepository $customerRepository
      */
-    public function personal(): Response
+    public function personal(CustomerRepository $customerRepository, Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('ui/account/personal.html.twig');
+        $customer = $customerRepository->findOneBy([]);
+
+//        $form = $this->createForm(CustomerType::class, $customer)->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $manager->flush();
+//
+//        }
+
+        return $this->render('ui/account/personal.html.twig', [
+            'customer' => $customer
+        ]);
     }
 
 }
