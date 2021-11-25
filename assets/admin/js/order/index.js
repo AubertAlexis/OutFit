1136b9dbddef90c26e51f9f7f007055e40ae045a
Jeremy.B <jeremy.breant@edu.itescia.fr>
2021-11-25 12:55:21 +0100

    read commandes

---
+++ assets/admin/js/order/index.js
@@ -0,0 +1,8 @@
+// Initialise datatable
+$(document).ready(function() {
+    $('#orderTable').DataTable({
+        "oLanguage": {
+            "sUrl": "//cdn.datatables.net/plug-ins/1.11.3/i18n/fr_fr.json"
+        }
+    });
+});
